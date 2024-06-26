d46d00e144ead70304617a984502116c7fffec66
Dzodzo188 <adrzazdzynski@edu.cdv.pl>
2024-06-26 18:24:38 +0200

    po merge

--- Quiz/frontend/src/App.jsx
+++ Quiz/frontend/src/App.jsx
@@ -1,18 +1,23 @@
 import { ConfigProvider } from "antd";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router, NavLink, Route, Routes} from "react-router-dom";
 import GameSetup from "./Components/GameSetup/GameSetup";
 import Game from "./Components/Game/Game";
 import { TimeProvider } from "./CustomHooks/TimeContext.jsx";
 import {QuestionsProvider} from "./CustomHooks/QuestionsContext.jsx";
+import Scoreboard from "./Components/Scoreboard/Scoreboard.jsx";
+import Index from "./Components/Index/Index.jsx";
 
 function App() {
     return (
         <ConfigProvider theme={{ token: { colorPrimary: "#45f321" } }}>
             <Router>
                 <Routes>
+                    <Route path="/">
+                    <Route path="/" element={<Index />} />
+                    <Route path="/scoreboard" element={<Scoreboard />} />
                     <Route
-                        path="/"
+                        path="/setup"
                         element={
                             <QuestionsProvider>
                                 <GameSetup />
@@ -29,6 +34,7 @@ function App() {
                             </QuestionsProvider>
                         }
                     />
+                    </Route>
                 </Routes>
             </Router>
         </ConfigProvider>
